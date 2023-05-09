53b2b4e7b5089e533d5a718dc37b983ae13fc088
Naitik Mehta <mnaitik4321@gmail.com>
2023-05-09 16:30:25 +0530

    we see how this point to global and user made object

--- THIS_in_javascript.js
+++ THIS_in_javascript.js
@@ -3,3 +3,19 @@
 // TODO part 1 for this keyword
 
 console.log(this);
+
+// For all regular function calls, this points to window object.
+
+console.log(this);
+
+
+var user = {    
+    firstName : "Naitik",
+    courseCount : 4,
+    getCourseCount : function () {  
+        console.log("Line 16", this);
+    },
+};
+user.getCourseCount();//this is not a regular function call this is a method call
+
+//so in regular function call this points to window object but in method call this points to the object which is calling the method
